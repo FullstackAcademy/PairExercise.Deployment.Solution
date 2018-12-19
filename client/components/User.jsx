be0f556e6983ebdc061a116a9357fa1cb16df047
Adam Sisk <adamsisk91@gmail.com>
2018-12-19 14:14:52 -0600

    travis test

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default (props) => (
+export default props => (
   <tr>
     <td className="user--first-name">{props.firstName}</td>
     <td className="user--last-name">{props.lastName}</td>
     <td className="user--email">{props.email}</td>
   </tr>
-)
+);
