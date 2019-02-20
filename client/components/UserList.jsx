8e40f561a4365ab9cd6af8a2f98fb41bc261aded
kylebirns <kylebirns@gmail.com>
2019-02-19 21:28:30 -0500

    cool

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -4,7 +4,7 @@ import User from './User.jsx'
 export default ({ users }) => (
   <div className="user-list">
 
-    <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
+    <p className='lead'>Here's a cool list of {users.length} completely fictitious people:</p>
 
     <table className="table">
       <tbody>
