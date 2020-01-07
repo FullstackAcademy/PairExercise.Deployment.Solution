f8e6701419930228d3f1bd676c967d2e7ef53af8
oneway101 <oneway.first@gmail.com>
2020-01-07 16:15:03 -0500

    testing depolyment

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -4,7 +4,7 @@ import User from './User.jsx'
 export default ({ users }) => (
   <div className="user-list">
 
-    <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
+    <p className='lead'>Here's a list of {users.length} completely amazing people:</p>
 
     <table className="table">
       <tbody>
