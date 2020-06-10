3aa7a5162e3289a8b9ae17fe81b1a7125b08ad25
Jianna Park <jiannajihyunpark@gmail.com>
2020-06-10 02:48:46 -0400

    return to initial commit

--- server/index.test.js
+++ server/index.test.js
@@ -4,30 +4,17 @@ const { expect } = require('chai')
 const db = require('./db')
 const seedUsers = require('../script/users.json')
 
-function onResponse(err, res) {
-  if (err) return done(err);
-  return done();
-}
 
 describe('GET /users', () => {
 
   before(async () => {
-    try {
-      await db.sync({ force: true })
-      await db.models.user.bulkCreate(seedUsers)
-    } catch (error) {
-      console.error('error syncing or creating db')
-    }
+    await db.sync({ force: true })
+    await db.models.user.bulkCreate(seedUsers)
   })
 
-  it('should return list of users', () => {
-    try {
-      request(app).get('/api/users').send((res) => {
-        expect(res.status).to.equal(200)
-        expect(res.body.length).to.equal(seedUsers.length)
-      }).end(onResponse)
-    } catch (error) {
-      console.error('error requesting get route')
-    }
+  it('should return list of users', async () => {
+    const res = await request(app).get('/api/users')
+    expect(res.status).to.equal(200)
+    expect(res.body.length).to.equal(seedUsers.length)
   })
 })
