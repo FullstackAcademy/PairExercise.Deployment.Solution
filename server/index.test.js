846ebfb3388d198eec0fa0a0035c775284640329
Jianna Park <jiannajihyunpark@gmail.com>
2020-06-10 02:08:18 -0400

    try with catch done

--- server/index.test.js
+++ server/index.test.js
@@ -7,15 +7,21 @@ const seedUsers = require('../script/users.json')
 
 describe('GET /users', () => {
 
-  before(() => {
-    return Promise.all([db.sync({ force: true }), db.models.user.bulkCreate(seedUsers)])
+  before((done) => {
+    Promise.all([db.sync({ force: true }), db.models.user.bulkCreate(seedUsers)])
+      .then(() => {
+        done()
+      })
+      .catch(done)
   })
 
-  it('should return list of users', async () => {
-    return request(app).get('/api/users')
+  it('should return list of users', (done) => {
+    request(app).get('/api/users')
       .then((res) => {
         expect(res.status).to.equal(200)
         expect(res.body.length).to.equal(seedUsers.length)
+        done()
       })
+      .catch(done)
   })
 })
