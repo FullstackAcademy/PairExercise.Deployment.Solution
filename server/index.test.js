157e05b42dc3be243d69fb14b6b1b4118a2e7a25
Jianna Park <jiannajihyunpark@gmail.com>
2020-06-10 02:14:04 -0400

    increase mocha timeout

--- server/index.test.js
+++ server/index.test.js
@@ -7,21 +7,14 @@ const seedUsers = require('../script/users.json')
 
 describe('GET /users', () => {
 
-  before((done) => {
-    Promise.all([db.sync({ force: true }), db.models.user.bulkCreate(seedUsers)])
-      .then(() => {
-        done()
-      })
-      .catch(done)
+  before(async () => {
+    await db.sync({ force: true })
+    await db.models.user.bulkCreate(seedUsers)
   })
 
-  it('should return list of users', (done) => {
-    request(app).get('/api/users')
-      .then((res) => {
-        expect(res.status).to.equal(200)
-        expect(res.body.length).to.equal(seedUsers.length)
-        done()
-      })
-      .catch(done)
+  it('should return list of users', async () => {
+    const res = await request(app).get('/api/users')
+    expect(res.status).to.equal(200)
+    expect(res.body.length).to.equal(seedUsers.length)
   })
 })
