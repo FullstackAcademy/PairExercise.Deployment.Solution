06aa15f0d7440c7b2362b3f17f7ff3711c390c19
Jianna Park <jiannajihyunpark@gmail.com>
2020-06-11 13:29:12 -0400

    testing test database

--- server/index.test.js
+++ server/index.test.js
@@ -5,16 +5,24 @@ const db = require('./db')
 const seedUsers = require('../script/users.json')
 
 
-// describe('GET /users', () => {
+describe('GET /users', () => {
 
-//   before(async () => {
-//     await db.sync({ force: true })
-//     await db.models.user.bulkCreate(seedUsers)
-//   })
+  before(async () => {
+    try {
+      await db.sync({ force: true })
+      await db.models.user.bulkCreate(seedUsers)
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
-//   it('should return list of users', async () => {
-//     const res = await request(app).get('/api/users')
-//     expect(res.status).to.equal(200)
-//     expect(res.body.length).to.equal(seedUsers.length)
-//   })
-// })
+  it('should return list of users', async () => {
+    try {
+      const res = await request(app).get('/api/users')
+      expect(res.status).to.equal(200)
+      expect(res.body.length).to.equal(seedUsers.length)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+})
