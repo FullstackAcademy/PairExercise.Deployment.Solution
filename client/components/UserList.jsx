96445063f0a16b3b5a56fae0d98bc8e4b85537f2
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-09-16 15:06:53 -0400

    edits UserList for testing travis deployment

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import User from './User.jsx'
+import React from 'react';
+import User from './User.jsx';
 
 export default ({ users }) => (
-  <div className="user-list">
+	<div className="user-list">
+		<p className="lead">Here's our list of {users.length} completely fictitious people:</p>
 
-    <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
-
-    <table className="table">
-      <tbody>
-        { users.map(u => <User {...u} key={u.id} />) }
-      </tbody>
-    </table>
-  </div>
-)
+		<table className="table">
+			<tbody>{users.map((u) => <User {...u} key={u.id} />)}</tbody>
+		</table>
+	</div>
+);
