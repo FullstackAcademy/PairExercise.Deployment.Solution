66c10bd789417eac1d6d2ab52774276e8da7cbc5
Kathryn Lam <katwingki@gmail.com>
2020-09-25 13:35:59 -0400

    Added h3 header to make sure the changes will be reflected on heroku

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import User from './User.jsx'
+import React from 'react';
+import User from './User.jsx';
 
 export default ({ users }) => (
-  <div className="user-list">
+  <div className='user-list'>
+    <h3>I AM UPDATED BY KATHRYN </h3>
+    <p className='lead'>
+      Here's a list of {users.length} completely fictitious people:
+    </p>
 
-    <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
-
-    <table className="table">
+    <table className='table'>
       <tbody>
-        { users.map(u => <User {...u} key={u.id} />) }
+        {users.map((u) => (
+          <User {...u} key={u.id} />
+        ))}
       </tbody>
     </table>
   </div>
-)
+);
