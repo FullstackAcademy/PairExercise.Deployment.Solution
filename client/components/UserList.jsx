27363f3c1c5f9acd6a88fc3410b29e733286347a
Kathryn Lam <katwingki@gmail.com>
2020-09-25 15:57:58 -0400

    Changes to Here's a list page

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -3,9 +3,9 @@ import User from './User.jsx';
 
 export default ({ users }) => (
   <div className='user-list'>
-    <h3>I AM UPDATED BY KATHRYN </h3>
     <p className='lead'>
-      Here's a list of {users.length} completely fictitious people:
+      Here's a list of {users.length} completely fictitious people ****** I AM
+      UPDATED BY KATHRYN:
     </p>
 
     <table className='table'>
