6b290ffe3ccf9b183916c09aa337e7f1cb49905b
Desiree Nelson <palebluedot4@gmail.com>
2020-12-23 07:54:54 -0500

    another try to get this working

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,7 @@ const pkg = require('../../package.json')
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 const client = new Sequelize(dbUrl, {
+  connectionString: process.nextTick.DATABASE_URL,
   logging: false,
   operatorsAliases: false,
   dialect: 'postgres',
