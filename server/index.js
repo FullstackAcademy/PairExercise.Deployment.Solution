c7af746b63670c15266dbfdc5d1116a7cd2cc655
Allyson Camitta <allyson.camitta@gmail.com>
2020-12-23 21:55:47 -0500

    configure travis

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const path = require('path')
-const db = require('./db')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const db = require('./db');
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/api/users', async (req, res, next) => {
-  try {
-    const users = await db.models.user.findAll()
-    res.json(users)
-  } catch (e) {
-    console.error(e)
-    next(e)
-  }
-})
+	try {
+		const users = await db.models.user.findAll();
+		res.json(users);
+	} catch (e) {
+		console.error(e);
+		next(e);
+	}
+});
 
-
-module.exports = app
+module.exports = app;
 
 if (require.main === module) {
-  app.listen(PORT, (e) => {
-    if (e) throw e
-    console.log(`listening on port ${PORT}`)
-  })
+	app.listen(PORT, e => {
+		if (e) throw e;
+		console.log(`listening on port ${PORT}`);
+	});
 }
