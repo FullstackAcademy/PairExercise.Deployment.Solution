8b29b5fa394985ea117c02ca90ed717977952864
Jenny Zeng <jzeng7077@gmail.com>
2020-12-29 20:56:54 -0500

    another testing commit

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -3,9 +3,8 @@ import User from './User.jsx'
 
 export default ({ users }) => (
   <div className="user-list">
-
     <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
-
+    <p>Testing to see if this comes up..</p>
     <table className="table">
       <tbody>
         { users.map(u => <User {...u} key={u.id} />) }
